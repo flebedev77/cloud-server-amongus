9fee99f1605a449cb6d2c6e5cbb634b583a9bc7d
flebedev77 <flebedev77@outlook.com>
2024-10-24 23:01:35 +1030

    Removed log file saving

--- src/prox-chat/logger.js
+++ src/prox-chat/logger.js
@@ -1,10 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
-// Define the log file path
-const logFilePath = path.join(__dirname, "../../", "logs", 'app.log');
-
-// Function to log messages
 function log(message, level = 'INFO') {
     const timestamp = new Date().toISOString();
     const formattedMessage = `[${timestamp}] [${level}] ${message}`;
@@ -12,12 +5,6 @@ function log(message, level = 'INFO') {
     // Write to console
     console.log(formattedMessage);
 
-    // Append to log file
-    fs.appendFile(logFilePath, formattedMessage, (err) => {
-        if (err) {
-            console.error('Failed to write to log file:', err);
-        }
-    });
 }
 
 // Export the logger functions
